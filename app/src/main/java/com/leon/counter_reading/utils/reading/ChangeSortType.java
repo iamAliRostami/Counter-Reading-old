8f584b1c5200a0dd8a66622559f1b8db7580a873
Leon <ali.rostami33@gmail.com>
2024-02-13 12:17:40 +0330

    sort has been changed

--- app/src/main/java/com/leon/counter_reading/utils/reading/ChangeSortType.java
+++ app/src/main/java/com/leon/counter_reading/utils/reading/ChangeSortType.java
@@ -11,6 +11,7 @@ import com.leon.counter_reading.activities.ReadingActivity;
 import com.leon.counter_reading.di.view_model.CustomProgressModel;
 
 import java.util.Collections;
+import java.util.Comparator;
 
 public class ChangeSortType extends AsyncTask<Activity, Void, Void> {
     private final boolean sortType;
@@ -35,8 +36,12 @@ public class ChangeSortType extends AsyncTask<Activity, Void, Void> {
             Collections.sort(readingData.onOffLoadDtos, (o1, o2) -> o2.eshterak.compareTo(o1.eshterak));
             Collections.sort(readingDataTemp.onOffLoadDtos, (o1, o2) -> o2.eshterak.compareTo(o1.eshterak));
         } else {
+            Collections.sort(readingData.onOffLoadDtos, Comparator.comparing(o -> o.eshterak));
+            Collections.sort(readingDataTemp.onOffLoadDtos, Comparator.comparing(o -> o.eshterak));
+/*
             Collections.sort(readingData.onOffLoadDtos, (o1, o2) -> o1.eshterak.compareTo(o2.eshterak));
             Collections.sort(readingDataTemp.onOffLoadDtos, (o1, o2) -> o1.eshterak.compareTo(o2.eshterak));
+*/
         }
         ((ReadingActivity) (activities[0])).setupViewPager(false);
         return null;
